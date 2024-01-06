7d99b977f2d6269110ae85572814fbf567d28b62
gaba-teixeira <gaba-teixeira@users.noreply.github.com>
2024-01-05 23:14:52 -0800

    Added API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function showWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let city = response.data.city;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "ab14t46ca824c44f19c53892o77f5c04";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchImput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchImput.value;
+
+  searchCity(searchImput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
