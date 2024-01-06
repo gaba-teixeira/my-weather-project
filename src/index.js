0288afd550a90301c2e450c680f8327d37ec8cc4
gaba-teixeira <gaba-teixeira@users.noreply.github.com>
2024-01-06 00:06:35 -0800

    Updated the time

--- src/index.js
+++ src/index.js
@@ -5,13 +5,41 @@ function showWeather(response) {
   let conditionElement = document.querySelector("#conditions");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector(`#wind`);
+  let timeElement = document.querySelector(`#current-time`);
+  let date = new Date(response.data.time * 1000);
 
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   conditionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  timeElement.innerHTML = `${changeTime(date)}, `;
 }
+
+function changeTime(date) {
+  let days = [
+    `Sunday`,
+    `Monday`,
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+  let day = days[date.getDay()];
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  return `${day}, ${hour}:${minutes}`;
+}
+
 function searchCity(city) {
   let apiKey = "ab14t46ca824c44f19c53892o77f5c04";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
